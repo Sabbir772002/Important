c6639c3730aacf1c8674fd37d6fe4f878ccaf1a5
Sabbir <Sabbir772002@gmail.com>
2022-10-21 02:00:01 +0600

    Code for database conection in java.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.sql.*;
+import java.util.Scanner;
+
+class MysqlCon{
+    public static void main(String args[]){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/college","root","");
+            Statement stmt=con.createStatement();
+            String s="select * from teacher";
+            String s1="select * from teacher where name='sabbir'";
+            ResultSet rs=stmt.executeQuery(s1);
+            PreparedStatement pt = con.prepareStatement("INSERT INTO teacher(Name,Id,Salary,Subject) VALUES(?,?,?,?)");
+            Scanner sc=new Scanner(System.in);
+            int i=5;
+            try {
+                while (i < 5) {
+                    i++;
+                    String name = sc.next();
+                    int id = sc.nextInt();
+                    int salary = sc.nextInt();
+                    String subject=sc.nextLine();
+                    pt.setString(1, name);
+                    pt.setString(4, subject);
+                    pt.setInt(3, salary);
+                    pt.setInt(2, id);
+                    pt.execute();
+
+
+                }
+
+            }catch(Exception e) { System.out.println("Exception");}
+            while(rs.next()){
+               // i++;
+
+                System.out.println(rs.getString(1) + "  " + rs.getInt(2) + "  " + rs.getInt(3) + " " + rs.getString(4));
+              //  if(i==6)break;
+            }
+            con.close();
+        }catch(Exception e){ System.out.println(e);}
     }
 }
